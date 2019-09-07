5dcf6a77ed7fb61512a9b943af44defff61e6322
Gaida M Escobar <gescobar129@gmail.com>
2019-09-07 02:21:37 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,6 +1,8 @@
 def square_array(array)
   counter = 0 
   while counter < array.length do
-    array[counter] 
-
+    new_array = array[counter] ** 2 
+    puts new_array
+    counter += 1
+  end 
 end
